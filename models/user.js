1902fe564b4987a1fcc0f5f0613ca42a57ffe314
ajeyajaz <ajeyajaz@gmail.com>
2026-01-20 19:02:27 +0530

    define the user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const joi = require('joi');
+
+// user
+const userSchema = new mongoose.Schema({
+    name:{type: String, required: true, maxLength: 255},
+    email:{type: String, required: true, maxLength: 255, unique: true},
+    password:{type: String, required: true, maxLength: 1024, minLength:8} // hashed password
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+function validateUser(value){
+
+    //schema based validation
+    const schema = joi.object({
+        name: joi.string().required().max(255),
+        email: joi.string().required().max(255).email(),
+        password: joi.string().required().min(8).max(50)
+    });
+
+    return {error, value} = schema.validate(value);
+}
+
+exports.User = User;
+exports.validate = validateUser;
